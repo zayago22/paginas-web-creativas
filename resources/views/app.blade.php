521637b15d8ab4499a3861378db1faa09aefe82d
Paginas Web Creativas <admin@paginaswebcreativas.com>
2026-02-24 20:22:46 -0600

    feat: add contact form section with lead capture

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="description" content="Diseñamos páginas web que generan clientes. +100 proyectos entregados en México.">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
